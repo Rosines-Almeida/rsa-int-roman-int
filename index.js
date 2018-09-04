16b0af3b728559e865a258da0aaa1bf8eceac8c2
rosines-almeida <rosines.almeida@gmail.com>
2018-09-04 08:41:06 -0300

    alterado funçao index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function intToRoman (num) {
+module.exports.romanToInt = function intToRoman (num) {
     const decimals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
 const roman = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
     for (var i = 0; i < decimals.length; i++) {
@@ -11,4 +11,28 @@ const roman = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV',
     }
 }
 
- module.exports = intToRoman;
+const objRoman = {
+    "I" : 1,
+    "V" : 5,
+    "X" : 10,
+    "L" : 50,
+    "C" : 100,
+    "D" : 500,
+    "M" : 1000
+};
+module.exports.intToRoman = function romanToInt(num) {
+
+var romanNumber = 0;
+for (var i = 0; i < num.length; i++) {
+if (objRoman[num[i]] < objRoman[num[i+1]]) {
+romanNumber -= objRoman[num[i]]; 
+} else {
+romanNumber += objRoman[num[i]];
+}
+}
+return romanNumber;
+};
+
+
+// module.exports = romanToInt;
+ 
--- test/roman.spec.js
+++ test/roman.spec.js
@@ -1,30 +1,30 @@
 const chai = require('chai');
-const intToRoman = require('../index.js');
-const romantoint = require('../romantoint.js');
-// const intToRoman = index.intToRoman;
+const index = require('../index.js');
 const expect = chai.expect;
- 
+const roman =  index.romanToInt;
+const int = index.intToRoman;
 
-describe('intToRoman()', function() {
+
+describe('romanToInt()', function() {
     it('Deve retornar 1 em algarismo romano', () => {
-        expect(intToRoman(1)).to.equal('I');
+        expect(roman(1)).to.equal('I');
         
       });
       it('Deve retornar 2 em algarismo romano', () => {
-      expect(intToRoman(2)).to.equal('II');
+      expect(roman(2)).to.equal('II');
         
   });
   it('Deve retornar 9 em  algarismo romano', () => {
-  expect(intToRoman(9)).to.equal('IX');
+  expect(roman(9)).to.equal('IX');
 });
 });
 
-describe('romantoint()', function() {
+describe('romanToInt()', function() {
     it('Deve retornar I em algarismo romano', () => {
-        expect(romantoint("I")).to.equal(1);
+        expect(int("I")).to.equal(1);
     });  
         it('Deve retornar V em algarismo romano', () => {
-            expect(romantoint("V")).to.equal(5);
+            expect(int("V")).to.equal(5);
         
 //   });
 //   it('Deve retornar 9 em  algarismo romano', () => {
